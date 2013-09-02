517a8305369c42be116838d6abd2d8b685b97c4e
Tomas Celaya <tjcelaya@gmail.com>
2013-09-02 00:39:38 -0400

    init maybe

--- setup.rb
+++ setup.rb
@@ -6,6 +6,7 @@ $PORT = ARGV[0] || 80
 use Rack::Deflater
 set :port, $PORT
 set :haml, :ugly => true
+
 Encoding.default_external = Encoding::UTF_8
 
 enable :sessions
@@ -14,14 +15,3 @@ get '/style.css' do
   headers "Content-Type" => "text/css;"
   stylus :style
 end
-
-
-# $MAILOPTS = {
-#     :address        => 'smtp.gmail.com',
-#     :port           => '25',
-#     :user_name      => 'grafulator@gmail.com',
-#     :password       => 'overachiever',
-#     :authentication => :plain 
-#       # :plain, :login, :cram_md5, no auth by default
-#   }
-
